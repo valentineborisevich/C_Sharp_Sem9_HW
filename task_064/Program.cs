f83d7f83dd7038c475dccf8e8c044ce833d39657
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-31 21:26:32 +0300

    tasks 64, 66, 68 added. task 64 finished

---
+++ task_064/Program.cs
@@ -0,0 +1,19 @@
+﻿// Все задачи решаются с помощью рекурсии
+// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNumbers(int number)
+{
+    if (number != 0)
+    {
+        Console.Write(number + " ");
+        PrintNumbers(number - 1);
+    }
+
+}
+
+Console.WriteLine("Enter the number");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(n);
---
+++ task_066/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void PrintNumbers(int number1, int number2)
+{
+    if (number1 == number2-1)
+    {
+        return;
+    }
+    PrintNumbers(number1-1, number2);
+    Console.Write(number1 + " ");
+}
+
+Console.WriteLine("Enter the number");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNumbers(m, n);
